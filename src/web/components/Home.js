6ae63a2d4f663829000d620eab83c642f5dfd080
Adam Pałka <adampalka1@op.pl>
2018-12-02 17:26:35 +0100

    production hosted on firebase server

--- src/web/components/Home.js
+++ src/web/components/Home.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Row, Col, Jumbotron } from 'reactstrap';
 import GoogleMap from './GoogleMap';
 import SearchBox from './SearchBox';
+import GMap from './GMap/GMap';
 
 const About = () => (
   <div>
     {/* <SearchBox /> */}
+    {/* <GMap /> */}
     <GoogleMap />
   </div>
 );
